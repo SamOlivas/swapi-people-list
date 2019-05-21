b75b13fc0aef115158832c7a776ba21f1fbe80d5
SamOlivas <sam.a.olivas@gmail.com>
2019-05-21 11:53:51 -0700

    some UI added

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const data = [];
-
-async function getPeople (data) {
-  const reply = await fetch ('https://swapi.co/api/people/')
-  const people = await reply.json()
-  return people
+const peopleList = document.getElementById('peopleList');
+const data = {
+  people: [],
 };
 
-console.log(data)
+async function getPeople () {
+  const reply = await fetch ('https://swapi.co/api/people/');
+  const people = await reply.json();
+  peopleArr = await people.results.map( person => person );
+  data.people = peopleArr;
+  render(peopleList, data.people);
+};
 
-const peopleList = document.getElementById('peopleList')
-console.log(peopleList)
+
+function render (element, data) {
+  element.innerHTML = `
+  <ul>
+  ${data.map( item =>`<li> ${item.name} </li>`
+  ).join('')}
+  </ul>
+  `;
+}
+
+
+peopleList.addEventListener('click', function (ev) {
+  let target = ev.target
+  console.log(target)
+})
+
+getPeople()
